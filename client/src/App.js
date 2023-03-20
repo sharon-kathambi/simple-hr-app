ee4ad1d08ea85512531ea82249c881cf48d1741f
Sharon Mwenda <smkathambi@gmail.com>
2023-03-20 14:38:32 +0300

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <label>Name</label>
+      <input type='text'></input>
+      <label>Age</label>
+      <input type='number'></input>
+      <label>Country</label>
+      <input type='text'></input>
+      <label>Position</label>
+      <input type='text'></input>
+      <label>Wage(year)</label>
+      <input type='number'></input>
+    </div>
+  );
+}
+
+export default App;
