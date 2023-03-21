e4dc8dfbe391447a4ee2a9de7a572e46bbe2758d
Sharon Mwenda <smkathambi@gmail.com>
2023-03-21 09:10:46 +0300

    feat:adding state to form inputs

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,43 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('')
+  const [age, setAge] = useState(0)
+  const [country, setCountry] = useState('')
+  const [position, setPosition] = useState('')
+  const [wage, setWage] = useState(0)
+
+
   return (
     <div className="App">
       <div className='information'>
       <label>Name</label>
-      <input type='text'></input>
+      <input 
+      type='text' 
+      onChange={(event)=> setName(event.target.value)}>
+      </input>
       <label>Age</label>
-      <input type='number'></input>
+      <input 
+      type='number'
+      onChange={(event)=> setAge(event.target.value)}
+      ></input>
       <label>Country</label>
-      <input type='text'></input>
+      <input 
+      type='text'
+      onChange={(event)=> setCountry(event.target.value)}
+      ></input>
       <label>Position</label>
-      <input type='text'></input>
+      <input 
+      type='text'
+      onChange={(event)=> setPosition(event.target.value)}
+      ></input>
       <label>Wage(year)</label>
-      <input type='number'></input>
-      <button>Add Employee</button>
-      
+      <input 
+      type='number'
+      onChange={(event)=> setWage(event.target.value)}></input>
+      <button >Add Employee</button>
+
       </div>
     </div>
   );
